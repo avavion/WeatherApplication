98fefddbbfa1640d38ab3fad74d9318557e48d01
avavion <avavionmvm@gmail.com>
2021-10-06 21:15:33 +0300

    Weather Component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Search from "./Search";
 import WeatherInfo from "./WeatherInfo";
 
-const URL = 'http://api.weatherapi.com/v1';
+const URL = 'https://api.weatherapi.com/v1';
 const API_KEY = "dedd2b41ffdb4854a5e203900210210";
 
 function Weather() {
@@ -13,13 +13,17 @@ function Weather() {
     const [searchValue, setSearchValue] = useState(LOCATION);
     const [weather, setWeather] = useState([]);
 
+    console.log(searchValue);
+
     useEffect(() => {
-        fetch(`${URL}/current.json?q=${searchValue}&key=${API_KEY}&lang=ru`)
+        fetch(`${URL}/current.json?q=${searchValue}&key=${API_KEY}&lang=ru`, { mode: "cors", method: "GET" })
             .then(r => r.json())
             .then(d => setWeather(d))
             .catch(e => console.error(e))
             .finally(() => console.log("Запрос успешно завершён"));
     }, [searchValue]);
+
+    console.log(weather);
 
     return (
         <section className="section">
